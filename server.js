a8c8c83a7f4daed36223b687eb2cc824dd4a9b18
saibot777 <stefantrajkovic777@gmail.com>
2018-01-18 22:23:09 +0100

    middlewares, mongo setup

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const flash = require('flash');
+const validator = require('express-validator');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const container = require('./modules');
 
+
 container.resolve(function(users) {
+
+    mongoose.Prmise = global.Promise;
+    mongoose.connect('mongodb://localhost/footballFans', { useMongoClient: true });
 
     const app = SetupExpress();
 
@@ -25,9 +35,20 @@ container.resolve(function(users) {
 
     function ConfigureExpress(app) {
         app.use(express.static('public'));
+        app.use(cookieParser());
         app.set('view engine', 'ejs');
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended: true}));
+
+        app.use(validator());
+        app.use(session({
+            secret: 'secretkey',
+            resave: 'true',
+            saveUninitialized: true,
+            store: new MongoStore({mongooseConnection: mongoose.connection})
+        }));
+
+        app.use(flash());
     }
 
 });
